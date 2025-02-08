24c88edc347b09764088121cb15f2e8b0f4591b6
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2025-02-08 13:03:32 -0600

    add home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import Card from "../../components/Card";
+import Label from "../../components/Label";
+
+import styles from "./page.module.css";
+
+import Pup from "../../assets/img/pup.svg";
 
 export default function Home() {
-  return <div>index</div>;
+  return (
+    <>
+      <div className={styles.page}>
+        <div className="row w-100">
+          <div className="col-md-3">
+            <Card>
+              <div>
+                <Label>Filter</Label>
+              </div>
+              <br />
+              <div></div>
+            </Card>
+          </div>
+          <div className="col-md-9 d-flex align-items-center justify-content-center">
+            <img src={Pup} className={styles.pup} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
