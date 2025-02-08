7d06a46872843b2786a0e81b08d3bc1b31cf10be
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2025-02-08 16:52:02 -0600

    update home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,33 @@
-import Card from "../../components/Card";
+import { useState } from "react";
+
+import { Dog } from "../../types/models";
+
 import Label from "../../components/Label";
 
 import styles from "./page.module.css";
-
 import Pup from "../../assets/img/pup.svg";
 
 export default function Home() {
+  const [dogs, setDogs] = useState<Dog[]>([]);
+
   return (
     <>
       <div className={styles.page}>
-        <div className="row w-100">
-          <div className="col-md-3">
-            <Card>
-              <div>
-                <Label>Filter</Label>
-              </div>
-              <br />
-              <div></div>
-            </Card>
-          </div>
-          <div className="col-md-9 d-flex align-items-center justify-content-center">
-            <img src={Pup} className={styles.pup} />
-          </div>
-        </div>
+        {dogs.length === 0 ? (
+          <>
+            <div className="d-flex align-items-center justify-content-center">
+              <img src={Pup} className={styles.pup} />
+            </div>
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{ fontWeight: 550 }}
+            >
+              <Label>Start by searching dog breeds</Label>
+            </div>
+          </>
+        ) : (
+          <>{JSON.stringify(dogs)}</>
+        )}
       </div>
     </>
   );
