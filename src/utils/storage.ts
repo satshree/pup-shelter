6c3c67547eb650cbcb014eb2ca0f28c6a5758618
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2025-02-08 13:40:50 -0600

    add local storage utils

---
+++ src/utils/storage.ts
@@ -0,0 +1,11 @@
+export function loadFromLocalStorage(key: string) {
+  const data = localStorage.getItem(key);
+
+  if (data) return JSON.parse(data);
+
+  return null;
+}
+
+export function saveToLocalStorage(key: string, value: any) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
