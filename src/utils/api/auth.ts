04827881792f44d09a2cf7fba03d36cd6ae1c338
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2025-02-08 15:55:58 -0600

    update api utils

--- src/utils/api/auth.ts
+++ src/utils/api/auth.ts
@@ -1,18 +1,32 @@
 import moment from "moment";
 
-import { loadFromLocalStorage, saveToLocalStorage } from "../storage";
+import { loginAPI, logoutAPI } from "../../api/auth";
 
-import { loginAPI } from "../../api/auth";
 import { AuthData } from "../../types/api/auth";
+
+import {
+  loadFromLocalStorage,
+  removeFromLocalStorage,
+  saveToLocalStorage,
+} from "../storage";
 
 const AUTH_KEY = "auth";
 const AUTH_EXPIRE_KEY = "authExpire";
 
 export async function authenticateUser(data: AuthData) {
-  await loginAPI(data);
+  const response = await loginAPI(data);
 
   saveToLocalStorage(AUTH_KEY, data);
   saveToLocalStorage(AUTH_EXPIRE_KEY, moment().add(1, "hours").format("X"));
+
+  return response;
+}
+
+export async function endUserSession() {
+  removeFromLocalStorage(AUTH_KEY);
+  removeFromLocalStorage(AUTH_EXPIRE_KEY);
+
+  await logoutAPI();
 }
 
 export function isLoggedIn() {
@@ -20,7 +34,7 @@ export function isLoggedIn() {
 
   if (!authExpire) return false;
 
-  if (Number(authExpire) > Number(moment().format("X"))) return false; // expired
+  if (Number(authExpire) < Number(moment().format("X"))) return false; // expired
 
   return true;
   //   const localAuthData = loadFromLocalStorage(AUTH_KEY);
