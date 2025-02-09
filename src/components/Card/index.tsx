6a6fd362ae747660af7b049a3968bcf78664b865
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2025-02-08 18:01:59 -0600

    add img option to card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,19 @@
 interface CardProps {
   children: React.ReactNode;
+  img?: string;
 }
 
 export default function Card(props: CardProps) {
   return (
     <div className="card" style={{ borderRadius: 12 }}>
+      {props.img ? (
+        <img
+          src={props.img}
+          className="card-img-top"
+          alt="doggo"
+          style={{ height: "200px", objectFit: "cover" }}
+        />
+      ) : null}
       <div className="card-body">{props.children}</div>
     </div>
   );
